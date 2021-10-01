da10797ed93fe45b550d8e02648e5dc5068d076e
Amin Soltani <asoltani927@gmail.com>
2021-10-01 17:22:44 +0330

    Changes to be committed: 	modified:   .gitignore 	modified:   src/App.css 	modified:   src/Minter.js  Changes not staged for commit: 	modified:   ../nft-minter/src/Minter.js 	modified:   ../nft-minter/src/util/interact.js

--- minter-starter-files/src/Minter.js
+++ minter-starter-files/src/Minter.js
@@ -1,112 +1,119 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import {
-  connectWallet,
-  getCurrentWalletConnected //import here
+    connectWallet,
+    getCurrentWalletConnected //import here
 } from "./utils/interact.js";
-import { mintNFT } from "./utils/interact.js";
+import {mintNFT} from "./utils/interact.js";
 
 const Minter = (props) => {
 
-  //State variables
-  const [walletAddress, setWallet] = useState("0xB9DB1902083cFbeeC905937e8e21e1b701267A17");
-  const [status, setStatus] = useState("");
-  const [name, setName] = useState("Test");
-  const [description, setDescription] = useState("test2");
-  const [count, setCount] = useState("1");
-  const [url, setURL] = useState("test3");
+    //State variables
+    const [walletAddress, setWallet] = useState("0xB9DB1902083cFbeeC905937e8e21e1b701267A17");
+    const [status, setStatus] = useState("");
+    const [name, setName] = useState("Test");
+    const [description, setDescription] = useState("test2");
+    const [count, setCount] = useState("1");
+    const [url, setURL] = useState("test3");
 
-  useEffect(async () => { //TODO: implement
-    const { address, status } = await getCurrentWalletConnected();
-    setWallet(address)
-    setStatus(status);
+    useEffect(async () => { //TODO: implement
+        const {address, status} = await getCurrentWalletConnected();
+        setWallet(address)
+        setStatus(status);
 
-    addWalletListener();
-  }, []);
+        addWalletListener();
+    }, []);
 
-  const connectWalletPressed = async () => {
-    const walletResponse = await connectWallet();
-    setStatus(walletResponse.status);
-    setWallet(walletResponse.address);
-  };
+    const connectWalletPressed = async () => {
+        const walletResponse = await connectWallet();
+        setStatus(walletResponse.status);
+        setWallet(walletResponse.address);
+    };
 
-  const onMintPressed = async () => {
-    const { status } = await mintNFT(url, name, description, count);
-    setStatus(status);
-  };
+    const onMintPressed = async () => {
+        const {status} = await mintNFT(url, name, description, count);
+        setStatus(status);
+    };
 
-  function addWalletListener() {
-    if (window.ethereum) {
-      window.ethereum.on("accountsChanged", (accounts) => {
-        if (accounts.length > 0) {
-          setWallet(accounts[0]);
-          setStatus("");
+    function addWalletListener() {
+        if (window.ethereum) {
+            window.ethereum.on("accountsChanged", (accounts) => {
+                if (accounts.length > 0) {
+                    setWallet(accounts[0]);
+                    setStatus("");
+                } else {
+                    setWallet("");
+                    setStatus("🦊 Connect to Metamask using the top right button.");
+                }
+            });
         } else {
-          setWallet("");
-          setStatus("🦊 Connect to Metamask using the top right button.");
+            setStatus(
+                <p>
+                    {" "}
+                    🦊{" "}
+                    <a target="_blank" href={`https://metamask.io/download.html`}>
+                        You must install Metamask, a virtual Ethereum wallet, in your
+                        browser.
+                    </a>
+                </p>
+            );
         }
-      });
-    } else {
-      setStatus(
-        <p>
-          {" "}
-          🦊{" "}
-          <a target="_blank" href={`https://metamask.io/download.html`}>
-            You must install Metamask, a virtual Ethereum wallet, in your
-            browser.
-          </a>
-        </p>
-      );
     }
-  }
 
-  return (
-    <div className="container mt-5">
+    return (
+        <div className="container mt-5">
 
-      <div class="row justify-content-center">
+            <div className="row justify-content-center">
 
-        <div class="col-12 col-lg-5">
+                <div className="col-12 col-lg-6">
 
-          <div>
+                    <div className="row">
+                        <div className="col-12">
+                            <button className="btn btn-outline-light" id="walletButton" onClick={connectWalletPressed}>
+                                {walletAddress.length > 0 ? (
+                                    "Connected: " +
+                                    String(walletAddress).substring(0, 6) +
+                                    "..." +
+                                    String(walletAddress).substring(38)
+                                ) : (
+                                    <span>Connect Wallet</span>
+                                )}
+                            </button>
+                        </div>
 
-            <button class="btn btn-outline-light" id="walletButton" onClick={connectWalletPressed}>
-              {walletAddress.length > 0 ? (
-                "Connected: " +
-                String(walletAddress).substring(0, 6) +
-                "..." +
-                String(walletAddress).substring(38)
-              ) : (
-                <span>Connect Wallet</span>
-              )}
-            </button>
+                        <div className="col-12">
+                            <div className="mt-5 text-center">
+                                <img className="logo img-fluid" src={'/logo.png'} alt={'logo'}/>
+                            </div>
+                        </div>
 
-          </div>
+                        <div className="col-12">
 
-          <div class="mt-5">
-            <img class="logo img-fluid" src="/logo.png" />
-          </div>
-          <div class="mt-5">
-            <form>
-              <h2>Number of Mints:</h2>
-              <input
-                class="form-control"
-                type="number"
-                placeholder="1"
-                onChange={(event) => setCount(event.target.value)}
-              />
 
-            </form>
-          </div>
-          <button class="btn btn-primary w-100" id="mintButton" onClick={onMintPressed}>
-            Mint NFT
-          </button>
-          <p id="status">
-            {status}
-          </p>
+                            <div className="mt-5">
+                                <form>
+                                    <h2>Number of Mints:</h2>
+                                    <input
+                                        className="form-control"
+                                        type="number"
+                                        placeholder="1"
+                                        onChange={(event) => setCount(event.target.value)}
+                                    />
+
+                                </form>
+                            </div>
+                            <button className="btn btn-primary w-100" id="mintButton" onClick={onMintPressed}>
+                                Mint NFT
+                            </button>
+                            <p id="status">
+                                {status}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
-
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Minter;
